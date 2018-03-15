0faf503d80df43cf50fbb92258d022476b6edc5d
Jon Fortescue <jonfortescue@protonmail.com>
2018-03-15 02:51:59 -0700

    Made argparsing more sophisticated; added support for searching for only non-ASCII words.

--- wiktionary.py
+++ wiktionary.py
@@ -1,8 +1,12 @@
 import sys
 import re
+import argparse
 from lxml import etree
 import codecs
 
+#############################################
+# all of this can  essentially be ignored. it's just unicode compat
+#
 # without this, encoding issues cause errors
 reload(sys)
 sys.setdefaultencoding('utf-8')
@@ -15,17 +19,26 @@ if sys.platform == "win32":
         sys.exc_clear()  # could be just pass, of course
     else:
         del uniconsole # reduce pollution, not needed anymore
+#############################################
+
+def is_ascii(s):
+    return all(ord(char) < 128 for char in s)
+
+parser = argparse.ArgumentParser(description='Parse Wiktionary dump for multilingual words')
+parser.add_argument('dump_file', nargs=1)
+parser.add_argument("--nonascii", action="store_true")
+args = parser.parse_args()
 
 # obtained from https://dumps.wikimedia.org/enwiktionary/
 # provide a path via command line
-DUMP_FILE_PATH = "dumps/%s" % sys.argv[1]
+DUMP_FILE_PATH = "dumps/%s" % args.dump_file[0]
 
 articles = dict()
 count = 0
 
 for event, element in etree.iterparse(DUMP_FILE_PATH, tag="{http://www.mediawiki.org/xml/export-0.10/}page"):
     title = element.findtext("{http://www.mediawiki.org/xml/export-0.10/}title")
-    if (":" not in title and element.find("{http://www.mediawiki.org/xml/export-0.10/}redirect") is None):
+    if (":" not in title and not (args.nonascii and is_ascii(title)) and element.find("{http://www.mediawiki.org/xml/export-0.10/}redirect") is None):
         revision = element.find("{http://www.mediawiki.org/xml/export-0.10/}revision")
         text = revision.findtext("{http://www.mediawiki.org/xml/export-0.10/}text")
         langsRE = re.compile(r'(^|\s)==.*[^=]==(\s|$)')
